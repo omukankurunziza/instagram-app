c0db4625e4c816f5a8db61329dc3cb5170c3270d
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-03-26 15:22:41 +0200

    add forms.py

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Comment,Profile,Image
-from django.contrib.auth.forms import AuthenticationForm
+# from django.contrib.auth.forms import AuthenticationForm
 
 class ProfileForm(forms.ModelForm):
 	model = Profile
@@ -22,16 +22,11 @@ class CommentForm(forms.ModelForm):
 		
 		exclude = ['user','image',]
 
-class ImageForm(forms.ModelForm):
-	model = Image
-	image_name = forms.CharField(max_length = 30)
-	
-	# bio = forms.CharField(label='Image Caption',max_length=500)
-	# image_image = forms.ImageField(label = 'Image Field')
+class PhotosLetterForm(forms.Form):
+    name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
 
-
-class ImageUploadForm(forms.ModelForm):
-	class Meta:
-		model = Image
-		
-		exclude = ['user']
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['user']
